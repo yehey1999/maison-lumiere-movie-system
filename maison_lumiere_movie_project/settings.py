c627d2d6ad938083645f4838f65932561974d81c
Rosalijos Joshua <rosalijos.joshua1999@gmail.com>
2020-09-15 19:30:47 +0700

    add movie model

--- maison_lumiere_movie_project/settings.py
+++ maison_lumiere_movie_project/settings.py
@@ -81,8 +81,16 @@ WSGI_APPLICATION = 'maison_lumiere_movie_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'maison_lumiere',
+        'USER': 'root',
+        'PASSWORD': '', 
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.DateField(default=datetime.now())
+    director = models.charField(max_length=100)
+    price = models.FloatField(default=0.00)
+    no_items = models.IntegerField(default=1)
+    genres = models.CharField(max_length=255)
+    casts = models.CharField(max_length=255)
+    genres = models.CharField(max_length=255)
+    casts = models.CharField(max_length=255)
+    images = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(default=datetime.now())
+    updated_at = models.DateTimeField(default=datetime.now())
+    
+    class Meta:
+        db_table = "Movie"
