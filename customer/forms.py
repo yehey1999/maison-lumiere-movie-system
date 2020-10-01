679fc67db0d01df6ae693ea80820ea0c5d180da8
Wylen Joan Lee <wylenjoan@gmail.com>
2020-10-01 21:41:13 +0800

    customer CRUD operations

--- customer/forms.py
+++ customer/forms.py
@@ -4,4 +4,4 @@ from .models import *
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ('first_name', 'last_name', 'gender')
+        fields = ('first_name', 'last_name', 'gender', 'status', 'birthdate', 'street', 'barangay', 'city', 'province', 'zipcode')
--- customer/views.py
+++ customer/views.py
@@ -70,59 +70,25 @@ class CustomerSummaryView(View):
     def post(self, request):
         if request.method == 'POST':
             if 'deleteBtn' in request.POST:
-                print('clicked delete button')
                 customer_id = request.POST.get("customer_id")
-                print(customer_id)
                 delete_customer = Customer.objects.filter(person_ptr_id=customer_id).delete()
                 delete_person = Person.objects.filter(id = customer_id).delete()
-                print('record deleted')
+                return redirect('/customer')
 
             elif 'saveBtn' in request.POST:
-                print('clicked update button')
                 customer_id = request.POST.get("customer_id2")
-                print(customer_id)
-                first_name = request.POST.get("first_name")
-                middle_name = request.POST.get("middle_name")
-                last_name = request.POST.get("last_name")
-                gender = request.POST.get("gender")
-                status = request.POST.get("status")
-                birthdate = request.POST.get("birthdate")
-                street = request.POST.get("street")
-                barangay = request.POST.get("barangay")
-                city = request.POST.get("city")
-                province = request.POST.get("province")
-                zipcode = request.POST.get("zipcode")
-                spouse_name = request.POST.get("spouse_name")
-                spouse_occupation = request.POST.get("spouse_occupation")
-                no_children = request.POST.get("no_children")
-                image = ""
-                if request.FILES.get("image"):
-                    image = request.FILES.get("image")
-                
                 customer = Customer.objects.get(person_ptr_id=customer_id)
-                customer.first_name = first_name
-                customer.middle_name = middle_name
-                customer.last_name = last_name
-                customer.gender = gender
-                customer.status = status
-                customer.birthdate = birthdate
-                customer.street = street
-                customer.barangay = barangay
-                customer.city = city
-                customer.province = province
-                customer.zipcode = zipcode
-                customer.spouse_name = spouse_name
-                customer.spouse_occupation = spouse_occupation
-                customer.no_children = no_children
-                customer.image = image
-                
-                # update_customer = Customer.objects.filter(person_ptr_id=customer_id).update(first_name = first_name, middle_name = middle_name, last_name = last_name, 
-                #             gender = gender, status = status, birthdate = birthdate, street = street,
-                #             barangay = barangay, city = city, province = province, zipcode = zipcode,
-                #             spouse_name = spouse_name, spouse_occupation = spouse_occupation, no_children = no_children,
-                #             image = image)
-                # print(update_customer)
-                print('record updated')
-                customer.save()
+                form = CustomerForm(request.POST, instance=customer)
 
-        return redirect('/customer')
+                if form.is_valid():
+                    customer = form.save(commit=False)
+                    middle_name = request.POST.get("middle_name")
+                    spouse_name = request.POST.get("spouse_name")
+                    spouse_occupation = request.POST.get("spouse_occupation")
+                    no_children = request.POST.get("no_children")
+                    image = request.FILES.get("image", "")
+                    customer.save()
+                    return redirect('/customer')
+
+
+        return HttpResponse("error")
