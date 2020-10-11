3589defd3cc3baf6cdef0f17b14c17e83995f816
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2020-10-11 19:07:33 +0800

    small changes

--- customer/models.py
+++ customer/models.py
@@ -24,7 +24,6 @@ class Person(models.Model):
 
 class Customer(Person):
     image = models.ImageField(null=True, blank=True, upload_to = 'customers/')
-    email = models.EmailField(max_length = 50)
     created_at = models.DateField(default = datetime.now())
     updated_at = models.DateField(default = datetime.now())
 
